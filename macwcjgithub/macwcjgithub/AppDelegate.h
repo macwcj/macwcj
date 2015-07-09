89cc43fc2daf55d91b6a23cb65410144350d47b3
lanou3g <macwcj@foxmail.com>
2015-07-09 09:43:07 +0800

    添加新工程工程

---
+++ macwcjgithub/macwcjgithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  macwcjgithub
+//
+//  Created by lanou3g on 15/7/9.
+//  Copyright (c) 2015年 wcj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ macwcjgithub/macwcjgithubTests/macwcjgithubTests.m
@@ -0,0 +1,40 @@
+//
+//  macwcjgithubTests.m
+//  macwcjgithubTests
+//
+//  Created by lanou3g on 15/7/9.
+//  Copyright (c) 2015年 wcj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface macwcjgithubTests : XCTestCase
+
+@end
+
+@implementation macwcjgithubTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
